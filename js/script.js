e44c92aaf1a1c40965184bc360778d6c6afcf20c
farruxbek <farruxbektoxtanazarov9@gmail.com>
2023-03-12 13:34:50 +0500

    add url shortener button

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const elUrlShortenerForm = document.querySelector(".js-url-shortener__form");
+const elUrlShortenerResults = document.querySelector(".url-shortener__results");
+const elCopyShortLinkButton = document.querySelector(".js-copy-short-button");
+const elUrlShortener = document.querySelector(".url-shortener");
+
+if (elUrlShortenerForm) {
+  elUrlShortenerForm.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+    elUrlShortenerResults.classList.add("url-shortener__results-open");
+  });
+}
+if (elUrlShortener) {
+  elUrlShortener.addEventListener("click", function (evt) {
+    if (evt.target.matches(".js-copy-short-button")) {
+      // cheng button text
+      evt.target.textContent = "Copied!";
+      // cheng bacground-coler
+      evt.target.classList.add("button-result-copied");
+
+      // reset button text and class
+      setTimeout(function () {
+        evt.target.textContent = "Copy";
+        evt.target.classList.remove("button-result-copied");
+      }, 1000);
+    }
+  });
+}
