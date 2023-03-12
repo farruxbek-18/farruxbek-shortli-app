1687fe8311a02e61138defff24bd20ebcbf00773
farruxbek <farruxbektoxtanazarov9@gmail.com>
2023-03-12 18:35:20 +0500

    add sait footer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const elUrlShortenerForm = document.querySelector(".js-url-shortener__form");
+const elUrlShortenerForm = document.querySelector(".js-url-shortener-form");
 const elUrlShortenerResults = document.querySelector(".url-shortener__results");
 const elCopyShortLinkButton = document.querySelector(".js-copy-short-button");
 const elUrlShortener = document.querySelector(".url-shortener");
